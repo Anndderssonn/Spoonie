874c2b846f3b336b7427b1f25855342c35d1408d
Anderson Valencia <andersson@Anderssons-MacBook-Pro.local>
2025-05-24 18:41:34 -0500

    feat: Add app theme file and category screen layout

---
+++ lib/presentation/screens/categories/categories_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Pick your category')),
+      body: GridView.builder(
+        itemCount: 10,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        itemBuilder: (context, index) {
+          return Text('Text: $index');
+        },
+      ),
+    );
+  }
+}
